6fe629a14d4411f253182f425f048dc59388f26f
Nitin S <nitin.bs.999@gmail.com>
2025-05-03 20:56:41 +0530

    swap variables

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
 print("Hello Agent")
+a=10
+b="ten"
+c=d=e=20
+print(a)
+print(b)
+print(str(c))
+print(type(d))
+print(id(e))
---
+++ swapVariables.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+a, b = b, a
+print("a:", a)
+print("b:", b)
+c,d,e,f = 1, 2, 3, 4
+print("c:", c)
+print("d:", d)      
+print("e:", e)
+print("f:", f)
+# print("Hello Agent")
